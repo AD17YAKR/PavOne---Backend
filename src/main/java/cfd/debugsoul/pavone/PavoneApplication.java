f29da61a02ce161070c80ce21956f877a23cbd8e
ad17yakr <adityasingh.110601@gmail.com>
2025-11-09 03:05:42 +0530

    first commit

---
+++ src/main/java/cfd/debugsoul/pavone/PavoneApplication.java
@@ -0,0 +1,13 @@
+package cfd.debugsoul.pavone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PavoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PavoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cfd/debugsoul/pavone/PavoneApplicationTests.java
@@ -0,0 +1,13 @@
+package cfd.debugsoul.pavone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PavoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
